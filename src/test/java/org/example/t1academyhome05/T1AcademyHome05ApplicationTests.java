ffe8aba67807c16640ca86ab512006bae430af9b
alexsch1312-tech <alexsch1312@gmail.com>
2026-06-22 19:54:07 +0300

    Initial commit

---
+++ src/test/java/org/example/t1academyhome05/T1AcademyHome05ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.t1academyhome05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1AcademyHome05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
